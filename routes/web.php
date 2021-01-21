fb145a842d2b539312e26932f4261b4cc3da7631
Nguyễn Quang Huy <huy.nguyenquang@alta.com.vn>
2021-01-21 10:55:20 +0700

    Debug index code

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'message' => "Xin chào",
+        'date' => date(DateTime::ISO8601)
+    ];
 });
